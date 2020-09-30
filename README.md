77e6a10bca405aa2250dd4d4235efef50b3e3ba8
Terence Martin <github@nurdz.com>
2020-09-30 00:23:16 -0700

    Initial commit
    Initial version of the plugin from the Scopes and Scope Selectors video,
    packaged up for easier use.

---
+++ README.md
@@ -0,0 +1,47 @@
+# ScopeView
+
+This is a simple package that allows you to display the syntax scope of the
+current character as well as visualizing the parts of the buffer that match a
+particular scope selector.
+
+This is an enhanced version of the plugin used in the [Scopes and Scope Selectors](https://youtu.be/37pcH9aQ76I)
+video on my [YouTube Channel](https://youtube.com/c/OdatNurd).
+
+See that video for a demonstration on what this plugin does.
+
+## Installation
+
+You can manually install this as a package by cloning it directly into your
+`Packages` folder. Alternatively, you can also use the `Package Control: Add Repository`
+command from the command palette and give it the URL of this repository, which
+will make the package available via Package Control.
+
+
+### ScopeView: Toggle Scope Display
+
+Toggles display of the scope under the cursor on and off for the currently
+active `view`. The display of the scope is controlled by the following settings:
+
+* `restore_at_startup` controls whether or not views that had this turned on when
+you quit Sublime still have it turned on when you restart Sublime. The default
+is to have this turned on.
+
+* `display_scope_at_eof` controls whether the inline scope display appears at
+the bottom of the file or on the line under the first caret in the file. The
+default for this is the line under the file, but for clarity in the YouTube
+video this was turned on.
+
+### ScopeView: View Scopes (alt+shift+s)
+
+Prompts you for a scope selector, and then marks the content of the file to show
+what text (if any) matches that scope. If nothing matches, the status bar will
+tell you (and nothing else exciting happens).
+
+Scopes are displayed in the color of the first matching scope (as determined by
+your color scheme). Matched scopes do not persist if you close and reopen the
+file or quit and restart Sublime.
+
+### ScopeView: Clear Scopes (alt+shift+c)
+
+Clears away the marked scopes that were previously added, if any. This is safe
+(and does nothing) if no scopes are being displayed.
---
+++ scope_view.py
@@ -0,0 +1,169 @@
+import sublime
+import sublime_plugin
+
+
+def plugin_loaded():
+    """
+    At plugin load, update the scope display for all views that had scopes
+    turned on previously.
+    """
+    settings = sublime.load_settings("ScopeView.sublime-settings")
+    restore = settings.get("restore_at_startup", True)
+
+    settings.add_on_change('scope_view', _settings_change)
+
+    for window in sublime.windows():
+        for view in window.views():
+            if view.settings().get("_display_scopes", False):
+                if restore:
+                    _display_scope(view)
+                else:
+                    view.settings().erase("_display_scopes")
+
+
+def plugin_unloaded():
+    """
+    At plugin unload, clear any displayed scopes from all views that have them.
+    """
+    settings = sublime.load_settings("ScopeView.sublime-settings")
+    settings.clear_on_change('scope_view')
+
+    for window in sublime.windows():
+        for view in window.views():
+            view.run_command("clear_scopes")
+            view.erase_phantoms("scopes")
+
+
+def _settings_change():
+    """
+    Whenever our settings change, redisplay all scopes so that if the location
+    setting changes, it's reflected immediately.
+    """
+    for window in sublime.windows():
+        for view in window.views():
+            if view.settings().get("_display_scopes", False):
+                _display_scope(view)
+
+
+def _display_scope(view):
+    """
+    Display the scope of the character under the selection as a phantom.
+    """
+    if not hasattr(_display_scope, "buffer_table"):
+        _display_scope.buffer_table = {}
+
+    # Obtain the phantom set used for this view
+    buff_id = view.buffer_id()
+    phantoms = _display_scope.buffer_table.get(buff_id, None)
+    if phantoms is None:
+        phantoms = sublime.PhantomSet(view, "scopes")
+        _display_scope.buffer_table[buff_id] = phantoms
+
+    scope_text = view.scope_name(view.sel()[0].b)
+
+    settings = sublime.load_settings("ScopeView.sublime-settings")
+    if settings.get("display_scope_at_eof", False):
+        phantom_region = sublime.Region(view.line(view.size()).a)
+    else:
+        phantom_region = sublime.Region(view.line(view.sel()[0].b).a)
+
+    bg_color = view.style()["background"]
+
+    phantoms.update([
+        sublime.Phantom(
+            phantom_region,
+            """
+                <body id="scope_line">
+                    <style>
+                        div.scope {{
+                            margin-top: 0.5rem;
+                            border-top: 0.1rem solid color(var(--greenish));
+                            background-color: {bg_color};
+                        }}
+                    </style>
+                    <div class="scope">{scope}</span>
+                </body>
+            """.format(bg_color=bg_color, scope=scope_text.replace(" ", "<br>")),
+            sublime.LAYOUT_BELOW
+            )
+        ])
+
+
+class ScopeInputHandler(sublime_plugin.TextInputHandler):
+    """
+    Prompt the user for a scope selector to match against.
+    """
+    def __init__(self, initial):
+        self.initial = initial.strip()
+
+    def placeholder(self):
+        return "scope selector"
+
+    def initial_text(self):
+        return self.initial
+
+
+class ViewScopesCommand(sublime_plugin.TextCommand):
+    """
+    Find all matches for the given scope selector and add them to a region set
+    in the buffer.
+    """
+    last_scope=""
+
+    def run(self, edit, scope):
+        self.last_scope = scope
+
+        matches = self.view.find_by_selector(scope)
+        if matches:
+            self.view.add_regions("scope_matches", matches, scope,
+                                  "dot", sublime.DRAW_NO_FILL)
+        else:
+            self.view.run_command("clear_scopes")
+            sublime.status_message("No matching scopes found")
+
+    def input(self, args):
+        if args.get("scope", None) is None:
+            return ScopeInputHandler(self.last_scope)
+
+    def input_description(self):
+        return "View scopes matching:"
+
+
+class ClearScopesCommand(sublime_plugin.TextCommand):
+    """
+    Clear all scopes that might currently be displayed by a previous execution
+    of view_scopes.
+    """
+    def run(self, edit):
+        self.view.erase_regions("scope_matches")
+
+
+class ToggleScopeDisplayCommand(sublime_plugin.TextCommand):
+    """
+    Toggle the display of the scope under the cursor on and off. Turning the
+    display on will immediately update the phantom, while turning it off will
+    hide the existing phantom.
+
+    The scope will only be displayed for the first selection caret.
+    """
+    def run(self, edit):
+        display = not self.view.settings().get("_display_scopes", False)
+        self.view.settings().set("_display_scopes", display)
+
+        if display:
+            _display_scope(self.view)
+        else:
+            self.view.erase_phantoms("scopes")
+
+
+class ScopeDisplayListener(sublime_plugin.ViewEventListener):
+    """
+    For any view that has scope display turned on, update the display whenver
+    the selection changes.
+    """
+    @classmethod
+    def is_applicable(cls, settings):
+        return settings.get("_display_scopes")
+
+    def on_selection_modified(self):
+        _display_scope(self.view)
